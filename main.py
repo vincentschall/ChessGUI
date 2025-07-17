89274d405babac8c0535b2f7e9562b77b665908d
Vincent Schall <v.schall@stud.unibas.ch>
2025-07-17 14:17:45 +0200

    better square selection

--- main.py
+++ main.py
@@ -86,12 +86,16 @@ class ChessGUI:
             move = chess.Move(self.selected_square, square)
             if move in self.board.legal_moves:
                 self.board.push(move)
-                self.selected_square = None                
+                self.selected_square = None     
                 self.draw_board()
                 self.root.after(200, self.engine_move)
-                self.moves.append(move);
+                self.moves.append(move);  
             else:
-                self.selected_square = None
+                piece = self.board.piece_at(square)
+                if piece and piece.color == chess.WHITE:
+                    self.selected_square = square
+                else:
+                    self.selected_square = None
                 self.draw_board()
 
     def engine_move(self):
